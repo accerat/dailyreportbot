037072ccfce7224d0c514c0b829919f6080c952c
Jorell <jorell@m-l-build.com>
2025-09-19 14:39:51 -0600

    v1 error trying to get bot back online

---
+++ src/startup-guards.js
@@ -0,0 +1,12 @@
+﻿process.on('unhandledRejection', (reason) => {
+  try { console.error('[unhandledRejection]', reason); } catch {}
+});
+process.on('uncaughtException', (err) => {
+  try { console.error('[uncaughtException]', err); } catch {}
+});
+
+// Env presence (true/false only)
+console.log('[env] APP_ID set:', !!process.env.APP_ID);
+console.log('[env] BOT_TOKEN set:', !!process.env.BOT_TOKEN);
+console.log('[env] PROJECT_DAILY_SUMMARIES_FORUM_ID set:', !!process.env.PROJECT_DAILY_SUMMARIES_FORUM_ID);
+console.log('[env] EXEC_SUMMARY_CHANNEL_ID set:', !!process.env.EXEC_SUMMARY_CHANNEL_ID);
